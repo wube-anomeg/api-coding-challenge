cf2f733ed9ce8d364e1cc74e11b769695570795e
Wube <wube@anomeg.com>
2023-10-06 10:23:42 -0400

    Create OpenAPIConfiguration.java

---
+++ src/main/java/com/mdbank/api/config/OpenAPIConfiguration.java
@@ -0,0 +1,20 @@
+package com.mdbank.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MD Bank API Documentation",
+                contact = @Contact(
+                        name = "Wube Kifle",
+                        email = "wube@anomeg.com"
+                ),
+                license = @License()),
+        servers = @Server(url = "http://localhost:8080")
+)
+public class OpenAPIConfiguration {
+}
